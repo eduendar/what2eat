fccbe553ce0dcba9c8e71d9b1499cbc9ee525109
Dündar <emre.duendar@accenture.com>
2017-09-15 13:53:14 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,33 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import * as firebase from 'firebase';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  user: Observable<firebase.User>
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar,
+     splashScreen: SplashScreen, afAuth: AngularFireAuth) {
+
+      this.user = afAuth.authState;
+      this.user.subscribe( (user) => {
+        console.log(user);
+        if (!user){
+          this.rootPage = LoginPage;
+        }else{
+          this.rootPage = TabsPage;
+        }
+      });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,36 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';  
+import { LoginPage } from "../pages/login/login";
+import { AuthService } from "./auth.service";
+
+// AF2 Settings
+export const firebaseConfig = {
+ apiKey: "AIzaSyCUY3-3sbpYxIgB878yQC4_Bn0xg2md5SQ",
+ authDomain: "what2eat-32cd4.firebaseapp.com",
+ databaseURL: "https://what2eat-32cd4.firebaseio.com",
+ projectId: "what2eat-32cd4",
+ storageBucket: "",
+ messagingSenderId: "87063529318"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +48,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}
